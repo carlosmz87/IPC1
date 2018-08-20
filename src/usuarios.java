71e1a72b80a7acdbe0cd21a1fbb65ad3b28d1e4f
carlosmz87 <carlosmartz1995@gmail.com>
2018-08-19 20:05:01 -0600

    commit final

--- src/usuarios.java
+++ src/usuarios.java
@@ -7,12 +7,20 @@
 /**
  *
  * @author carlosmartinez
+ * UNIVERSIDAD DE SAN CARLOS FACULTAD DE INGENIERIA
+ * IPC1
+ * CARLOS JAVIER MARTINEZ POLANCO 201709282
  */
+
+//DEFINIMOS LA CLASE USUARIOS
 public class usuarios {
+    
+    //ATRIBUTOS
     private int id;
     private String nombre;
     private int nota1, nota2, nota3, nota4, nota5, suma, promedio;
     
+    //SETTERS Y GETTERS
     public void setID(int id){
         this.id=id;
     }
@@ -20,6 +28,7 @@ public class usuarios {
     public void setNombre(String nombre){
         this.nombre=nombre;
     }
+    
     
     public void setNotas(int n1, int n2, int n3, int n4, int n5){
         this.nota1=n1;
@@ -43,15 +52,15 @@ public class usuarios {
         return nota1 + nota2 + nota3 + nota4 + nota5;
     
     }
-    
+    //LA FUNCION PROMEDIO QUE CALCULA EL PROMEDIO
    public int promedio(int n1, int n2, int n3, int n4, int n5){
        suma=n1+n2+n3+n4+n5;
        promedio= suma/5;
        return promedio;
    
    }
-    
+    //SE DEFINE EL OBJETO USUARIO POR MEDIO DE UN TOSTRING()
     public String toString(){
-        return "EL USUARIO:  " + id + "NOMBRE: " + nombre + "TIENE NOTAS DE: " + nota1 + nota2 + nota3 + nota4 + nota5 + "CON UN PROMEDIO DE:" + promedio;
+        return " EL USUARIO:  " + id + " NOMBRE: " + nombre + " TIENE NOTAS DE: " + nota1 +" " + nota2 + " " + nota3 + " " + nota4 + " " + nota5 + " " + " CON UN PROMEDIO DE:" + promedio;
     }
 }
